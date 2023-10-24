cc9813a35349d997cbc3a3e35d397d5744eebf1d
yunshu-cloud <w2099266050@gmail.com>
2023-10-24 14:49:56 +0800

    spring ioc container demo

--- src/main/java/com/yunshu/springioc/service/StudentService.java
+++ src/main/java/com/yunshu/springioc/service/StudentService.java
@@ -1,5 +1,6 @@
 package com.yunshu.springioc.service;
 
+import com.yunshu.springioc.Container;
 import com.yunshu.springioc.dao.StudentDao;
 import com.yunshu.springioc.dao.impl.StudentImpl;
 import com.yunshu.springioc.pojo.Student;
@@ -7,8 +8,24 @@ import com.yunshu.springioc.pojo.Student;
 public class StudentService {
 
 
+    /**
+     * 未使用ioc容器
+     * @param id
+     * @return
+     */
     public Student findById(int id){
         StudentDao studentDao = new StudentImpl();
+        return studentDao.findById(id);
+    }
+
+
+    /**
+     * 使用ioc容器
+     * @param id
+     * @return
+     */
+    public Student findByIdUsingContainer(int id){
+        StudentDao studentDao = (StudentDao) Container.getBean("studentDao");
         return studentDao.findById(id);
     }
 }
